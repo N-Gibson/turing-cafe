914a4f03cc70bdd075da7f4e57f12b43bb7c1ab4
Noah Gibson <noah.gibson99@gmail.com>
2019-10-17 11:43:04 -0600

    Try and fix form button simulation test

--- src/Form/Form.test.js
+++ src/Form/Form.test.js
@@ -3,12 +3,32 @@ import { shallow } from 'enzyme';
 import Form from './Form';
 
 describe('Form', () => {
-  let wrapper;
+  let wrapper, makeRes;
   beforeEach(() => {
-    wrapper = shallow(<Form />)
-  })
+    makeRes = jest.fn()
+    wrapper = shallow(<Form makeReservation={makeRes}/>)
+  });
 
   it('should match the snapshot', () => {
     expect(wrapper).toMatchSnapshot()
+  });
+
+  it('should be able to handle a change in state', () => {
+    const mockEvent = {
+      target: {
+        name: 'name',
+        value: 'Bob'
+      }
+    }
+    wrapper.instance().handleChange(mockEvent)
+
+    expect(wrapper.state('name')).toEqual('Bob')
+  });
+
+  it('should make a reservation on click', () => {
+    wrapper.find('button').simulate('click')
+
+    expect(makeRes.mock.calls.length).toEqual(1)
+    // expect(makeRes).toHaveBeenCalled()
   })
 })
